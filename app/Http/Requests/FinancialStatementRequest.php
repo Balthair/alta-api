bf07251b323b5a33b597e94adc02611deec5af79
Luis <luisfajardo0789@gmail.com>
2020-02-02 19:53:02 -0600

    Finishing Benefit program model

--- app/Http/Requests/FinancialStatementRequest.php
+++ app/Http/Requests/FinancialStatementRequest.php
@@ -8,23 +8,13 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class BenefitProgramRequest extends FormRequest
+class FinancialStatementRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
